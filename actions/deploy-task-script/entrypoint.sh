a74a46ebcd8b0033b2e8dc780410069d1d1024e8
Alex Clay <aclay@tetrascience.com>
2022-03-03 09:56:39 -0500

    more entrypoint updates

--- actions/deploy-task-script/entrypoint.sh
+++ actions/deploy-task-script/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+yarn version
 
 echo "${INPUT_SSH_KEY}" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
@@ -6,7 +7,13 @@ chmod 600 /root/.ssh/id_rsa
 
 echo "MOUNTED WORKSPACE: $GITHUB_WORKSPACE"
 
-cd /usr/abc_test/
+pushd /usr/abc_test/
 # git clone git@github.com/tetrascience/ts-lib-artifact-builder --depth=1
 yarn install --frozen-lockfile --prod
-yarn run publish $GITHUB_WORKSPACE
+yarn link
+
+popd
+pwd
+ls -altr
+yarn install
+yarn link abc_test
