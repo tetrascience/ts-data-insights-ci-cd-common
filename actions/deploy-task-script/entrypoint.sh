1185546d655641286e6922c019d278933f786edb
Alex Clay <aclay@tetrascience.com>
2022-03-03 09:42:31 -0500

    remove extra prints

--- actions/deploy-task-script/entrypoint.sh
+++ actions/deploy-task-script/entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-echo "--- ENV ---"
-env
 echo "${INPUT_SSH_KEY}" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 # cat /root/.ssh/id_rsa
 
-echo "--- ARGS ---"
-echo $*
 echo "MOUNTED WORKSPACE: $GITHUB_WORKSPACE"
 
 cd /usr/abc_test/
