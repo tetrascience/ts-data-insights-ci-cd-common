f667a4f2d0268cc12b013c8baa643009b05f23e4
Tarik Setia <setiat@tetrascience.com>
2022-06-27 11:14:57 +0530

    DE-4100: init tag-validator

--- .github/actions/deprecation-checker/test/snapshots/snap_test_deprecation_checker.py
+++ .github/actions/deprecation-checker/test/snapshots/snap_test_deprecation_checker.py
@@ -7,29 +7,29 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_integration 1'] = [
+snapshots["test_integration 1"] = [
     {
-        'column': 0,
-        'endColumn': None,
-        'endLine': None,
-        'line': 1,
-        'message': "Deprecated keyword argument file_category='IDS' passed to Context.write_file()",
-        'message-id': 'W1599',
-        'module': 'context_deprecation',
-        'obj': '',
-        'symbol': 'deprecated-context-api',
-        'type': 'warning'
+        "column": 0,
+        "endColumn": None,
+        "endLine": None,
+        "line": 1,
+        "message": "Deprecated keyword argument file_category='IDS' passed to Context.write_file()",
+        "message-id": "W1599",
+        "module": "context_deprecation",
+        "obj": "",
+        "symbol": "deprecated-context-api",
+        "type": "warning",
     },
     {
-        'column': 4,
-        'endColumn': None,
-        'endLine': None,
-        'line': 6,
-        'message': "Deprecated keyword argument file_category='IDS' passed to Context.write_file()",
-        'message-id': 'W1599',
-        'module': 'context_deprecation',
-        'obj': 'raw_to_ids',
-        'symbol': 'deprecated-context-api',
-        'type': 'warning'
-    }
+        "column": 4,
+        "endColumn": None,
+        "endLine": None,
+        "line": 6,
+        "message": "Deprecated keyword argument file_category='IDS' passed to Context.write_file()",
+        "message-id": "W1599",
+        "module": "context_deprecation",
+        "obj": "raw_to_ids",
+        "symbol": "deprecated-context-api",
+        "type": "warning",
+    },
 ]
---
+++ .github/actions/tag-validator/validate_tag.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+import sh
+import semver
+
+class Tag:
+    def __init__(self, tag: str, artifact_type: Optional[str] = None):
+
+        _tag = tag[1:] if tag.startswith("v") else tag
+        self.tag = semver.VersionInfo.parse(_tag)
+
+        self.artifact_type = artifact_type
+
+    def validate(self):
+        self.validate_tag_is_new()
+        self.validate_tag_is_not_reserved_for_pull_request()
+
+    def validate_tag_is_new(self):
+        existing_tags = sh.git("tag")
+        if existing_tags:
+            existing_tags = existing_tags.split("\n")
+            if self.tag in existing_tags:
+                raise ValueError(f"Tag {self.tag} already exist.")
+
+    def validate_tag_is_not_reserved_for_pull_request(self):
+        if (
+            self.tag.major == 0
+            and self.tag.minor == 0
+        ):
+            raise ValueError(
+                f"Tag: {str(self.tag)} is reserved for pull request."
+                "Major and minor version can not be zero."
+            )
